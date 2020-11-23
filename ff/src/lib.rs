f82f2465d86e72a691c4bf7bf4c5a9eb91a0b75a
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-11-23 12:44:33 -0800

    fmt

--- ff/src/lib.rs
+++ ff/src/lib.rs
@@ -8,7 +8,6 @@
 )]
 #![allow(clippy::op_ref, clippy::suspicious_op_assign_impl)]
 #![cfg_attr(not(use_asm), forbid(unsafe_code))]
-
 #![cfg_attr(use_asm, feature(llvm_asm))]
 #![cfg_attr(use_asm, deny(unsafe_code))]
 
