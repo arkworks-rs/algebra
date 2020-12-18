afe23b01a6aaeaaa35928509531dc86ef6b621ae
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2020-12-17 23:06:43 -0800

    Merge

--- poly/src/domain/general.rs
+++ poly/src/domain/general.rs
@@ -31,44 +31,6 @@ pub enum GeneralEvaluationDomain<F: FftField> {
     MixedRadix(MixedRadixEvaluationDomain<F>),
 }
 
-// TODO
-impl<F: FftField> CanonicalSerialize for GeneralEvaluationDomain<F> {
-    #[inline]
-    fn serialize<W: Write>(&self, mut writer: W) -> Result<(), SerializationError> {
-        match self {
-            GeneralEvaluationDomain::Radix2(domain) => {
-                0u8.serialize(&mut writer)?;
-                domain.serialize(&mut writer)?;
-            },
-            GeneralEvaluationDomain::MixedRadix(domain) => {
-                1u8.serialize(&mut writer)?;
-                domain.serialize(&mut writer)?;
-            }
-        }
-        Ok(())
-    }
-
-    #[inline]
-    fn serialized_size(&self) -> usize {
-        match self {
-            GeneralEvaluationDomain::Radix2(domain) => domain.serialized_size() + 1,
-            GeneralEvaluationDomain::MixedRadix(domain) => domain.serialized_size() + 1,
-        }
-    }
-}
-
-impl<F: FftField> CanonicalDeserialize for GeneralEvaluationDomain<F> {
-    #[inline]
-    fn deserialize<R: Read>(mut reader: R) -> Result<Self, SerializationError> {
-        let domain_type = u8::deserialize(&mut reader)?;
-        match domain_type {
-            0 => Ok(Self::Radix2(Radix2EvaluationDomain::deserialize(&mut reader)?)),
-            1 => Ok(Self::MixedRadix(MixedRadixEvaluationDomain::deserialize(&mut reader)?)),
-            _ => unreachable!(),
-        }
-    }
-}
-
 macro_rules! map {
     ($self:expr, $f1:ident $(, $x:expr)*) => {
         match $self {
