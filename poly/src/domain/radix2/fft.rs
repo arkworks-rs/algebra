ea1ef4bcb37ffb485055a1c38803023bc8eb925a
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-01-13 21:55:33 -0800

    Document what `roots_of_unity` computes (#175)
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- poly/src/domain/radix2/fft.rs
+++ poly/src/domain/radix2/fft.rs
@@ -60,21 +60,22 @@ impl<F: FftField> Radix2EvaluationDomain<F> {
         ark_std::cfg_iter_mut!(x_s).for_each(|val| *val *= self.size_inv);
     }
 
+    /// Computes the first `self.size / 2` roots of unity for the entire domain.
+    /// e.g. for the domain [1, g, g^2, ..., g^{n - 1}], it computes
+    // [1, g, g^2, ..., g^{(n/2) - 1}]
     #[cfg(not(feature = "parallel"))]
-    fn roots_of_unity(&self, root: F) -> Vec<F> {
-        compute_powers_serial(self.size as usize, root)
+    pub(super) fn roots_of_unity(&self, root: F) -> Vec<F> {
+        compute_powers_serial((self.size as usize) / 2, root)
     }
 
+    /// Computes the first `self.size / 2` roots of unity.
     #[cfg(feature = "parallel")]
-    pub(crate) fn roots_of_unity(&self, root: F) -> Vec<F> {
-        // TODO: Understand why this functions output isn't domain.elements(),
-        // but it still works.
-        // See if it can be altered to be in normal order, or to replace
-        // parallel compute powers.
+    pub(super) fn roots_of_unity(&self, root: F) -> Vec<F> {
+        // TODO: check if this method can replace parallel compute powers.
         let log_size = ark_std::log2(self.size as usize);
         // early exit for short inputs
         if log_size <= LOG_ROOTS_OF_UNITY_PARALLEL_SIZE {
-            compute_powers_serial(self.size as usize, root)
+            compute_powers_serial((self.size as usize) / 2, root)
         } else {
             let mut temp = root;
             // w, w^2, w^4, w^8, ..., w^(2^(log_size - 1))
