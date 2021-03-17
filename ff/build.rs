308452e2272bff11fb943b30ff3fd4f15e3d9517
jonch <9093549+jon-chuang@users.noreply.github.com>
2021-03-17 19:04:10 +0800

    remove remnants

--- ff/build.rs
+++ ff/build.rs
@@ -15,26 +15,4 @@ fn main() {
     if should_use_asm {
         println!("cargo:rustc-cfg=use_asm");
     }
-
-    let should_use_bw6_asm = cfg!(any(
-        all(
-            feature = "bw6_asm",
-            target_feature = "bmi2",
-            target_feature = "adx",
-            target_arch = "x86_64"
-        ),
-        feature = "force_bw6_asm"
-    ));
-    if should_use_bw6_asm {
-        cc::Build::new()
-            .file("bw6-assembly/modmul768-sos1-adx.S")
-            .compile("modmul768");
-        cc::Build::new()
-            .file("bw6-assembly/modadd768.S")
-            .compile("modadd768");
-        cc::Build::new()
-            .file("bw6-assembly/modsub768.S")
-            .compile("modsub768");
-        println!("cargo:rustc-cfg=use_bw6_asm");
-    }
 }
