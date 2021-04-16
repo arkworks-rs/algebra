db0ca4658f1f692a65da3d3aa61a6221ac7f0c54
François Garillot <francois@garillot.net>
2021-04-16 17:09:00 -0400

    Adds `From<iXXX>`  implementations to quadratic / cubic field extensions
    Those delegate to the respective, existing `From<uXXX>` implementations for `XXX` in `[8, 16, 32, 64, 128]`

--- ff/src/fields/models/quadratic_extension.rs
+++ ff/src/fields/models/quadratic_extension.rs
@@ -435,9 +435,35 @@ impl<P: QuadExtParameters> From<u128> for QuadExtField<P> {
     }
 }
 
+impl<P: QuadExtParameters> From<i128> for QuadExtField<P> {
+    #[inline]
+    fn from(val: i128) -> Self {
+        let positivity = val.is_positive();
+        let abs = Self::from(val.unsigned_abs());
+        if positivity {
+            abs
+        } else {
+            -abs
+        }
+    }
+}
+
 impl<P: QuadExtParameters> From<u64> for QuadExtField<P> {
     fn from(other: u64) -> Self {
         Self::new(other.into(), P::BaseField::zero())
+    }
+}
+
+impl<P: QuadExtParameters> From<i64> for QuadExtField<P> {
+    #[inline]
+    fn from(val: i64) -> Self {
+        let positivity = val.is_positive();
+        let abs = Self::from(val.unsigned_abs());
+        if positivity {
+            abs
+        } else {
+            -abs
+        }
     }
 }
 
@@ -447,15 +473,54 @@ impl<P: QuadExtParameters> From<u32> for QuadExtField<P> {
     }
 }
 
+impl<P: QuadExtParameters> From<i32> for QuadExtField<P> {
+    #[inline]
+    fn from(val: i32) -> Self {
+        let positivity = val.is_positive();
+        let abs = Self::from(val.unsigned_abs());
+        if positivity {
+            abs
+        } else {
+            -abs
+        }
+    }
+}
+
 impl<P: QuadExtParameters> From<u16> for QuadExtField<P> {
     fn from(other: u16) -> Self {
         Self::new(other.into(), P::BaseField::zero())
     }
 }
 
+impl<P: QuadExtParameters> From<i16> for QuadExtField<P> {
+    #[inline]
+    fn from(val: i16) -> Self {
+        let positivity = val.is_positive();
+        let abs = Self::from(val.unsigned_abs());
+        if positivity {
+            abs
+        } else {
+            -abs
+        }
+    }
+}
+
 impl<P: QuadExtParameters> From<u8> for QuadExtField<P> {
     fn from(other: u8) -> Self {
         Self::new(other.into(), P::BaseField::zero())
+    }
+}
+
+impl<P: QuadExtParameters> From<i8> for QuadExtField<P> {
+    #[inline]
+    fn from(val: i8) -> Self {
+        let positivity = val.is_positive();
+        let abs = Self::from(val.unsigned_abs());
+        if positivity {
+            abs
+        } else {
+            -abs
+        }
     }
 }
 
