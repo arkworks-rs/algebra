d89ed196b67be7e9600d454e0ab77f53619e4ac2
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-08-12 15:26:45 -0700

    Fix build script

--- ff/build.rs
+++ ff/build.rs
@@ -5,16 +5,15 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
     let is_nightly = version_meta().expect("nightly check failed").channel == Channel::Nightly;
-
     let asm_enabled = cfg!(feature = "asm");
-    let bmi2_enabled  = cfg!(target_feature = "bmi2");
-    let adx_enabled  = cfg!(target_feature = "adx");
-    let is_x86_64  = cfg!(target_arch = "x86_64");
-    // Left in for debugging purposes.
-    // assert!(asm_enabled, "failed asm check");
-    // assert!(is_x86_64, "failed x86_64 check");
-    // assert!(adx_enabled, "failed adx check");
-    // assert!(bmi2_enabled, "failed bmi2 check");
+
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_features = std::env::var("CARGO_CFG_TARGET_FEATURE").unwrap_or("".into());
+    let target_features = target_features.split(",").collect::<Vec<_>>();
+    let bmi2_enabled  = target_features.contains(&"bmi2");
+    let adx_enabled  = target_features.contains(&"adx");
+
+    let is_x86_64  = target_arch == "x86_64";
     let should_use_asm = asm_enabled && bmi2_enabled && adx_enabled && is_x86_64 && is_nightly;
     if should_use_asm {
         println!("cargo:rustc-cfg=use_asm");
