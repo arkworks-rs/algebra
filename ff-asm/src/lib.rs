27d266ee35c6b2e96a7348ea1894eb35b745b86b
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-08-12 14:52:55 -0700

    add comments

--- ff-asm/src/lib.rs
+++ ff-asm/src/lib.rs
@@ -166,7 +166,7 @@ fn generate_llvm_asm_mul_string(
         )
     };
 
-    let _comment = |comment: &str| {
+    let comment = |comment: &str| {
         llvm_asm_string
             .borrow_mut()
             .push_str(&format!("         // {}", comment));
@@ -214,6 +214,7 @@ fn generate_llvm_asm_mul_string(
 
     macro_rules! mul_1 {
         ($a:expr, $b:ident, $zero:ident, $limbs:expr) => {
+            comment("Mul 1 start");
             movq($a, RDX);
             mulxq($b[0], R[0], R[1]);
             for j in 1..$limbs - 1 {
@@ -224,11 +225,13 @@ fn generate_llvm_asm_mul_string(
             movq($zero, RBX);
             adcxq(RAX, R[$limbs - 1]);
             adcxq(RBX, RCX);
+            comment("Mul 1 end")
         };
     }
 
     macro_rules! mul_add_1 {
         ($a:ident, $b:ident, $zero:ident, $i:ident, $limbs:expr) => {
+            comment(&format!("mul_add_1 start for iteration {}", $i));
             movq($a[$i], RDX);
             for j in 0..$limbs - 1 {
                 mulxq($b[j], RAX, RBX);
@@ -240,11 +243,13 @@ fn generate_llvm_asm_mul_string(
             adcxq(RAX, R[($i + $limbs - 1) % $limbs]);
             adoxq(RBX, RCX);
             adcxq(RBX, RCX);
+            comment(&format!("mul_add_1 end for iteration {}", $i));
         };
     }
 
     macro_rules! mul_add_shift_1 {
         ($a:ident, $mod_prime:ident, $zero:ident, $i:ident, $limbs:expr) => {
+            comment(&format!("mul_add_shift_1 start for iteration {}", $i));
             movq($mod_prime, RDX);
             mulxq(R[$i], RDX, RAX);
             mulxq($a[0], RAX, RBX);
@@ -260,6 +265,7 @@ fn generate_llvm_asm_mul_string(
             adcxq(RAX, R[($i + $limbs - 1) % $limbs]);
             adoxq(RCX, R[$i % $limbs]);
             adcxq(RBX, R[$i % $limbs]);
+            comment(&format!("mul_add_shift_1 end for iteration {}", $i));
         };
     }
     begin();
@@ -327,7 +333,7 @@ mod tests {
     fn expand_muls() {
         let impl_block = super::generate_impl(4, true);
         println!("{}", impl_block);
-        let impl_block = super::generate_impl(6, true);
-        println!("{}", impl_block);
+        // let impl_block = super::generate_impl(6, true);
+        // println!("{}", impl_block);
     }
 }
--- ff/build.rs
+++ ff/build.rs
@@ -6,12 +6,16 @@ fn main() {
 
     let is_nightly = version_meta().expect("nightly check failed").channel == Channel::Nightly;
 
-    let should_use_asm = cfg!(all(
-        feature = "asm",
-        target_feature = "bmi2",
-        target_feature = "adx",
-        target_arch = "x86_64"
-    )) && is_nightly;
+    let asm_enabled = cfg!(feature = "asm");
+    let bmi2_enabled  = cfg!(target_feature = "bmi2");
+    let adx_enabled  = cfg!(target_feature = "adx");
+    let is_x86_64  = cfg!(target_arch = "x86_64");
+    // Left in for debugging purposes.
+    // assert!(asm_enabled, "failed asm check");
+    // assert!(is_x86_64, "failed x86_64 check");
+    // assert!(adx_enabled, "failed adx check");
+    // assert!(bmi2_enabled, "failed bmi2 check");
+    let should_use_asm = asm_enabled && bmi2_enabled && adx_enabled && is_x86_64 && is_nightly;
     if should_use_asm {
         println!("cargo:rustc-cfg=use_asm");
     }
