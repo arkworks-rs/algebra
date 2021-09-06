d58e295194b1764169ae4a61629669ef4a4723ec
Simon Masson <simon.masson@yahoo.fr>
2021-09-06 18:30:00 +0200

    adds for subgroup checks

--- ec/src/models/short_weierstrass_jacobian.rs
+++ ec/src/models/short_weierstrass_jacobian.rs
@@ -137,8 +137,7 @@ impl<P: Parameters> GroupAffine<P> {
     /// Checks if `self` is in the subgroup having order that equaling that of
     /// `P::ScalarField`.
     pub fn is_in_correct_subgroup_assuming_on_curve(&self) -> bool {
-        self.mul_bits(BitIteratorBE::new(P::ScalarField::characteristic()))
-            .is_zero()
+        P::is_in_correct_subgroup_assuming_on_curve(self)
     }
 }
 
