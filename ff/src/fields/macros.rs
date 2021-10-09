0f8668d524fa1c3035a1687f493c59ff039451b0
Marcin Górny <marcin.gorny.94@protonmail.com>
2021-10-09 11:55:55 +0200

    Document definitions in macros

--- ff/src/fields/macros.rs
+++ ff/src/fields/macros.rs
@@ -88,6 +88,11 @@ macro_rules! impl_prime_field_serializer {
 
 macro_rules! impl_Fp {
     ($Fp:ident, $FpParameters:ident, $BigInteger:ident, $BigIntegerType:ty, $limbs:expr, $field_size:expr) => {
+        /// Parameters of the prime field of size at most
+        #[doc = $field_size]
+        /// invariably tied to the BigInteger struct comprising
+        #[doc = $limbs]
+        /// that holds the internal representation of the field elements.
         pub trait $FpParameters: FpParameters<BigInt = $BigIntegerType> {}
 
         /// Represents an element of the prime field F_p, where `p == P::MODULUS`.
@@ -112,6 +117,8 @@ macro_rules! impl_Fp {
         );
 
         impl<P> $Fp<P> {
+            /// Construct a new prime element directly from its underlying
+            /// BigInteger data type.
             #[inline]
             pub const fn new(element: $BigIntegerType) -> Self {
                 Self(element, PhantomData)
