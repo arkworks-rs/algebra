0d64f63eb3f32c2b3bb83ac06801d5394d8e0c77
weikeng <w.k@berkeley.edu>
2021-12-05 18:19:00 -0800

    fmt

--- test-curves/src/bls12_381/tests.rs
+++ test-curves/src/bls12_381/tests.rs
@@ -6,8 +6,8 @@ use crate::bls12_381::{g1, Fq, Fq2, Fq6, FqParameters, Fr, G1Affine, G1Projectiv
 use ark_algebra_test_templates::{
     curves::*, fields::*, generate_field_test, generate_g1_test, groups::*, msm::*,
 };
-use ark_std::{rand::Rng, test_rng};
 use ark_std::ops::{AddAssign, MulAssign, SubAssign};
+use ark_std::{rand::Rng, test_rng};
 
 generate_field_test!(bls12_381; fq2; fq6;);
 generate_g1_test!(bls12_381; curve_tests; sw_tests;);
