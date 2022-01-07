287a97dbf189b413d5b2b6794fc2d6bc45938fcf
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-01-07 12:52:24 -0800

    Revert "Add benchmarks to `test-curves` (#354)"
    This reverts commit dd96c8a18ef4a20bcd563fbbd95e5825a56ff3eb.

--- poly-benches/benches/dense_multilinear.rs
+++ poly-benches/benches/dense_multilinear.rs
@@ -12,7 +12,7 @@ const NUM_VARIABLES_RANGE: Range<usize> = 10..21;
 fn arithmetic_op_bench<F: Field>(c: &mut Criterion) {
     let mut rng = test_rng();
 
-    let mut group = c.benchmark_group("AddDense");
+    let mut group = c.benchmark_group("Add");
     for nv in NUM_VARIABLES_RANGE {
         group.bench_with_input(BenchmarkId::new("Add", nv), &nv, |b, &nv| {
             let poly1 = DenseMultilinearExtension::<F>::rand(nv, &mut rng);
@@ -22,7 +22,7 @@ fn arithmetic_op_bench<F: Field>(c: &mut Criterion) {
     }
     group.finish();
 
-    let mut group = c.benchmark_group("SubDense");
+    let mut group = c.benchmark_group("Sub");
     for nv in NUM_VARIABLES_RANGE {
         group.bench_with_input(BenchmarkId::new("Sub", nv), &nv, |b, &nv| {
             let poly1 = DenseMultilinearExtension::<F>::rand(nv, &mut rng);
@@ -35,7 +35,7 @@ fn arithmetic_op_bench<F: Field>(c: &mut Criterion) {
 
 fn evaluation_op_bench<F: Field>(c: &mut Criterion) {
     let mut rng = test_rng();
-    let mut group = c.benchmark_group("EvaluateDense");
+    let mut group = c.benchmark_group("Evaluate");
     for nv in NUM_VARIABLES_RANGE {
         group.bench_with_input(BenchmarkId::new("evaluate", nv), &nv, |b, &nv| {
             let poly = DenseMultilinearExtension::<F>::rand(nv, &mut rng);
