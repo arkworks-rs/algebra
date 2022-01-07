781ff1838ad7dcb8d7b3369395b02b587f35d04b
Weikeng Chen <w.k@berkeley.edu>
2022-01-07 01:16:31 -0800

    Update lib.rs

--- ff/src/lib.rs
+++ ff/src/lib.rs
@@ -9,6 +9,8 @@
 #![allow(clippy::op_ref, clippy::suspicious_op_assign_impl)]
 #![cfg_attr(not(feature = "asm"), forbid(unsafe_code))]
 #![cfg_attr(feature = "asm", deny(unsafe_code))]
+#![allow(stable_features)]
+#![cfg_attr(use_asm, feature(asm))]
 #![cfg_attr(use_asm, feature(asm_const))]
 
 #[macro_use]
