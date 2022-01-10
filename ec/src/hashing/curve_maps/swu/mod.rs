61305906207233e2e5a2b3078ff15c4e18a3b893
drskalman <35698397+drskalman@users.noreply.github.com>
2022-01-10 17:14:15 -0500

    Improve comments related to constants of SWU map parameters.
    Co-authored-by: Pratyush Mishra <pratyushmishra@berkeley.edu>

--- ec/src/hashing/curve_maps/swu/mod.rs
+++ ec/src/hashing/curve_maps/swu/mod.rs
@@ -14,13 +14,15 @@ use crate::{
 ///
 /// - [\[WB2019\]] <https://eprint.iacr.org/2019/403>
 pub trait SWUParams: SWModelParameters {
-    // we need an element of the base field which is not a square root see [1] Sect.
-    // 4. it is also convenient to have $g(b/xi * a)$ to be square. In general
-    // we use a xi with low absolute value coefficients when they are
-    // represented as element of ZZ.
-    const XI: Self::BaseField; // a nonsquare in Fq
-    const ZETA: Self::BaseField; // arbitatry root of unity
-    const XI_ON_ZETA_SQRT: Self::BaseField; // square root of THETA
+    /// An element of the base field that is not a square root see \[WB2019, Section 4\]. 
+    /// It is also convenient to have $g(b/xi * a)$ to be square. In general
+    /// we use a `XI` with low absolute value coefficients when they are
+    /// represented as integers.
+    const XI: Self::BaseField;
+    /// An arbitrary root of unity in the base field.
+    const ZETA: Self::BaseField;
+    /// Square root of `THETA = Self::XI/Self::ZETA`.
+    const XI_ON_ZETA_SQRT: Self::BaseField;
 }
 
 /// Represents the SWU hash-to-curve map defined by `P`.
