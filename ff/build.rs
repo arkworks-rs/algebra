2d3358608bb84d0d20f9f5df8f1221e7d468135f
weikengchen <w.k@berkeley.edu>
2022-02-05 00:10:09 -0800

    keep the asm feature

--- ff/build.rs
+++ ff/build.rs
@@ -5,6 +5,11 @@ fn main() {
 
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
 
+    let rust_version = rustc_version::version().unwrap();
+    let is_asm_stable = rust_version >= Version::new(1, 59, 0);
+
+    let asm_enabled = cfg!(feature = "asm");
+
     let is_x86_64 = target_arch == "x86_64";
     let mut bmi2_enabled = false;
     let mut adx_enabled = false;
@@ -13,17 +18,7 @@ fn main() {
         adx_enabled = is_x86_feature_detected!("adx");
     }
 
-    let rust_version = rustc_version::version().unwrap();
-    println!("rust_version = {}", rust_version);
-    let is_asm_stable = rust_version >= Version::new(1, 59, 0);
-
-    println!("is_asm_stable = {}", is_asm_stable);
-    if is_x86_64 {
-        println!("bmi2_enabled = {}", bmi2_enabled);
-        println!("adx_enabled = {}", adx_enabled);
-    }
-
-    let should_use_asm = is_asm_stable && bmi2_enabled && adx_enabled && is_x86_64;
+    let should_use_asm = is_asm_stable && asm_enabled && bmi2_enabled && adx_enabled && is_x86_64;
     if should_use_asm {
         println!("cargo:rustc-cfg=use_asm");
     }
