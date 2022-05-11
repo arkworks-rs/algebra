0e6b24635cea2a8bec1be71647269c9c0a7ad39f
Marcin Górny <marcin.gorny.94@protonmail.com>
2022-05-11 12:43:36 +0200

    Bring fq12 and g2 from bls12-381 to test-curves

--- test-curves/src/bls12_381/mod.rs
+++ test-curves/src/bls12_381/mod.rs
@@ -1,5 +1,6 @@
 pub mod fr;
 pub use fr::*;
+use ark_ec::bls12::{Bls12, Bls12Parameters, TwistType};
 
 #[cfg(feature = "bls12_381_curve")]
 pub mod fq;
@@ -8,9 +9,29 @@ pub mod fq2;
 #[cfg(feature = "bls12_381_curve")]
 pub mod fq6;
 #[cfg(feature = "bls12_381_curve")]
+pub mod fq12;
+#[cfg(feature = "bls12_381_curve")]
 pub mod g1;
+#[cfg(feature = "bls12_381_curve")]
+pub mod g2;
 
 #[cfg(feature = "bls12_381_curve")]
-pub use {fq::*, fq2::*, fq6::*, g1::*};
+pub use {fq::*, fq2::*, fq6::*, fq12::*, g1::*, g2::*};
 #[cfg(test)]
 mod tests;
+
+pub type Bls12_381 = Bls12<Parameters>;
+
+pub struct Parameters;
+
+impl Bls12Parameters for Parameters {
+    const X: &'static [u64] = &[0xd201000000010000];
+    const X_IS_NEGATIVE: bool = true;
+    const TWIST_TYPE: TwistType = TwistType::M;
+    type Fp = Fq;
+    type Fp2Config = Fq2Config;
+    type Fp6Config = Fq6Config;
+    type Fp12Config = Fq12Config;
+    type G1Parameters = self::g1::Parameters;
+    type G2Parameters = self::g2::Parameters;
+}
