64e282d29ecd27fa693529855b5d5427260e4885
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-07-05 19:01:54 -0700

    Improve `MontFp` macro

--- test-curves/src/bls12_381/fq.rs
+++ test-curves/src/bls12_381/fq.rs
@@ -6,11 +6,13 @@ use ark_ff::fields::{Fp384, MontBackend};
 pub struct FqConfig;
 pub type Fq = Fp384<MontBackend<FqConfig, 6>>;
 
-pub const FQ_ONE: Fq = ark_ff::MontFp!(Fq, "1");
-pub const FQ_ZERO: Fq = ark_ff::MontFp!(Fq, "0");
+pub const FQ_ONE: Fq = ark_ff::MontFp!("1");
+pub const FQ_ZERO: Fq = ark_ff::MontFp!("0");
 
 #[cfg(test)]
 mod tests {
+    use core::marker::PhantomData;
+
     use super::*;
     use ark_ff::BigInt;
     #[test]
@@ -85,14 +87,17 @@ mod tests {
         // 2758230843577277949620073511305048635578704962089743514587482222134842183668501798417467556318533664893264801977679
         assert_eq!(
             FqConfig::GENERATOR,
-            Fq::new(BigInt::new([
-                0x321300000006554f,
-                0xb93c0018d6c40005,
-                0x57605e0db0ddbb51,
-                0x8b256521ed1f9bcb,
-                0x6cf28d7901622c03,
-                0x11ebab9dbb81e28c,
-            ]))
+            ark_ff::Fp(
+                BigInt::new([
+                    0x321300000006554f,
+                    0xb93c0018d6c40005,
+                    0x57605e0db0ddbb51,
+                    0x8b256521ed1f9bcb,
+                    0x6cf28d7901622c03,
+                    0x11ebab9dbb81e28c,
+                ]),
+                PhantomData
+            )
         );
     }
 }
