c7a8ee6b40d2d5192e774707a0c16e318e960417
mmagician <marcin.gorny.94@protonmail.com>
2022-11-30 10:12:24 +0100

    fmt

--- test-curves/src/fp128.rs
+++ test-curves/src/fp128.rs
@@ -1,13 +1,11 @@
 //! Prime field `Fp` where `p = 2^127 - 1`.
 use ark_ff::fields::{Fp128, MontBackend};
 
-
 #[derive(ark_ff::MontConfig)]
 #[modulus = "170141183460469231731687303715884105727"]
 #[generator = "43"]
 pub struct FqConfig;
 pub type Fq = Fp128<MontBackend<FqConfig, 2>>;
-
 
 #[cfg(test)]
 mod tests {
