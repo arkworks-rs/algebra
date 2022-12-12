3e626022a0310b2bdbfa898f07816211c996d57b
Achim Schneider <achim@parity.io>
2022-12-12 10:41:25 +0100

    fmt

--- ec/src/models/short_weierstrass/group.rs
+++ ec/src/models/short_weierstrass/group.rs
@@ -636,10 +636,7 @@ impl<P: SWCurveConfig> ScalarMul for Projective<P> {
 }
 
 impl<P: SWCurveConfig> VariableBaseMSM for Projective<P> {
-    fn msm(
-        bases: &[Self::MulBase],
-        bigints: &[Self::ScalarField],
-    ) -> Result<Self, usize> {
+    fn msm(bases: &[Self::MulBase], bigints: &[Self::ScalarField]) -> Result<Self, usize> {
         P::model_msm(bases, bigints)
     }
 }
