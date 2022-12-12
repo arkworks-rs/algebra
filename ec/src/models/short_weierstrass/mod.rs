a7126e5aac28ae3a3d59e678f80150f544b52e89
Achim Schneider <achim@parity.io>
2022-12-12 09:17:54 +0100

    msm_bigint_default in SWCurveConfig

--- ec/src/models/short_weierstrass/mod.rs
+++ ec/src/models/short_weierstrass/mod.rs
@@ -110,7 +110,7 @@ pub trait SWCurveConfig: super::CurveConfig {
         bases: &[Affine<Self>],
         bigints: &[<Self::ScalarField as PrimeField>::BigInt],
     ) -> Projective<Self> {
-        VariableBaseMSM::default_msm_bigint(bases, bigints)
+        VariableBaseMSM::msm_bigint_default(bases, bigints)
     }
 
     /// If uncompressed, serializes both x and y coordinates as well as a bit for whether it is
