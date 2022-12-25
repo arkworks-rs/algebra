4836abfd98820f9b113ddd57f8e442e14aaec3a9
Michele Orrù <michele.orru@berkeley.edu>
2022-12-25 08:57:44 +0100

    Remove poly-benches.

--- poly/benches/dense_uv_polynomial.rs
+++ poly/benches/dense_uv_polynomial.rs
@@ -1,13 +1,14 @@
 extern crate criterion;
+mod common;
 
 use ark_ff::{FftField, Field};
 use ark_poly::{
     polynomial::univariate::{DensePolynomial, SparsePolynomial},
     DenseUVPolynomial, EvaluationDomain, GeneralEvaluationDomain, Polynomial,
 };
-use ark_poly_benches::size_range;
 use ark_std::rand::Rng;
 use ark_test_curves::bls12_381::Fr as bls12_381_fr;
+use common::size_range;
 use criterion::{criterion_group, criterion_main, Bencher, BenchmarkId, Criterion};
 
 const BENCHMARK_MIN_DEGREE: usize = 1 << 15;
