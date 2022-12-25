e117c2b6aa971ecf420fdaf829c825e8b9bf560b
mmagician <marcin.gorny.94@protonmail.com>
2022-12-25 16:41:15 +0100

    fix method docs

--- ff/src/biginteger/arithmetic.rs
+++ ff/src/biginteger/arithmetic.rs
@@ -131,7 +131,7 @@ pub fn mac_with_carry(a: u64, b: u64, c: u64, carry: &mut u64) -> u64 {
     tmp as u64
 }
 
-/// Calculate a + 2*(b * c) + carry_lo  (carry_hi << 64), returning the least significant digit
+/// Calculate a + 2*(b * c) + carry_lo + (carry_hi << 64), returning the least significant digit
 /// and setting carry0 to the 2nd-most significant digit, and carry1 to most significant digit
 /// TODO carry1 could be a bool/u8?
 #[inline(always)]
