72a41c6e6104e50c06efe4ad81b8ba539de6347a
Simon Masson <simon.masson@yahoo.fr>
2023-01-03 12:22:22 +0100

    push files WIP

--- ec/src/scalar_mul/glv.rs
+++ ec/src/scalar_mul/glv.rs
@@ -1,62 +1,172 @@
-use crate::{CurveConfig, CurveGroup};
+use core::ops::{Mul, Neg};
+
+use crate::{
+    short_weierstrass::Affine, AffineRepr, CurveConfig, CurveGroup, ScalarMul, VariableBaseMSM,
+};
+use ark_ff::PrimeField;
+use num_bigint::BigUint;
 
 /// The GLV parameters for computing the endomorphism and scalar decomposition.
 pub trait GLVConfig: Send + Sync + 'static + CurveConfig {
-    /// A representation of curve points that enables efficient arithmetic by
-    /// avoiding inversions.
-    type Curve: CurveGroup<Config = Self>;
+    // /// A representation of curve points that enables efficient arithmetic by
+    // /// avoiding inversions.
+    // type Curve: CurveGroup<Config = Self>;
 
     // Constants that are used to calculate `phi(G) := lambda*G`.
 
-    /// Coefficient `a_1` of `f(y) = a_1 * (y + a_2) * (y + a_3)`.
-    const COEFF_A1: Self::BaseField;
-    /// Coefficient `a_2` of `f(y) = a_1 * (y + a_2) * (y + a_3)`.
-    const COEFF_A2: Self::BaseField;
-    /// Coefficient `a_3` of `f(y) = a_1 * (y + a_2) * (y + a_3)`.
-    const COEFF_A3: Self::BaseField;
+    /// Coefficients for the CM endomorphism
+    const COEFFS_ENDOMORPHISM: &'static [Self::BaseField];
 
-    /// Coefficient `b_1` of `g(y) = b_1 * (y + b_2) * (y + b_3)`.
-    const COEFF_B1: Self::BaseField;
-    /// Coefficient `b_2` of `g(y) = b_1 * (y + b_2) * (y + b_3)`.
-    const COEFF_B2: Self::BaseField;
-    /// Coefficient `b_3` of `g(y) = b_1 * (y + b_2) * (y + b_3)`.
-    const COEFF_B3: Self::BaseField;
-
-    /// Coefficient `c_1` of `h(y) = (y + c_1) * (y + c_2)`.
-    const COEFF_C1: Self::BaseField;
-    /// Coefficient `c_2` of `h(y) = (y + c_1) * (y + c_2)`.
-    const COEFF_C2: Self::BaseField;
+    /// LAMBDA the eigenvalue corresponding to the endomorphism
+    const LAMBDA: Self::ScalarField;
 
     // Constants for scalar decomposition.
     // This is a 2x2 matrix, which is practically the LLL-reduced bases.
+    // We require this matrix N to satisfy det(N) = ScalarField::characteristic().
 
-    /// The first element of the matrix for scalar decomposition.
-    const COEFF_N11: Self::ScalarField;
-    /// The second element of the matrix for scalar decomposition.
-    const COEFF_N12: Self::ScalarField;
-    /// The third element of the matrix for scalar decomposition.
-    const COEFF_N21: Self::ScalarField;
-    /// The forth element of the matrix for the scalar decomposition.
-    const COEFF_N22: Self::ScalarField;
+    const COEFF_N: [<Self as CurveConfig>::ScalarField; 4];
+    const SGN_N: [bool; 4];
 
-    /// Maps a point G to phi(G):= lambda G where psi is the endomorphism.
-    // On an affine curve, the function takes the following steps:
-    //  f(y) = a_1 * (y + a_2) * (y + a_3)
-    //  g(y) = b_1 * (y + b_2) * (y + b_3)
-    //  h(y) = (y + c_1) * (y + c_2)
-    //  return (x',y') where
-    //  x' = x * f(y) / y
-    //  y' = g(y) / h(y)
-    fn endomorphism(
-        base: &<Self::Curve as CurveGroup>::Affine,
-    ) -> <Self::Curve as CurveGroup>::Affine;
+    // /// Maps a point G to phi(G):= lambda G where psi is the endomorphism.
+    // fn endomorphism(
+    //     base: &<Self::Curve as CurveGroup>::Affine,
+    // ) -> <Self::Curve as CurveGroup>::Affine;
 
     /// Decomposes a scalar s into k1, k2, s.t. s = k1 + lambda k2,
-    fn scalar_decomposition(k: &Self::ScalarField) -> (Self::ScalarField, Self::ScalarField);
+    fn scalar_decomposition(
+        k: Self::ScalarField,
+    ) -> (
+        <Self as CurveConfig>::ScalarField,
+        bool,
+        <Self as CurveConfig>::ScalarField,
+        bool,
+    ) {
+        let scalar: BigUint = k.into_bigint().into();
 
-    /// Performs GLV multiplication.
-    fn glv_mul(
-        base: &<Self::Curve as CurveGroup>::Affine,
-        scalar: &Self::ScalarField,
-    ) -> Self::Curve;
+        let (sgn_n11, n11): (bool, BigUint) = (Self::SGN_N[0], Self::COEFF_N[0].into());
+        let (sgn_n12, n12): (bool, BigUint) = (Self::SGN_N[1], Self::COEFF_N[1].into());
+        let (sgn_n21, n21): (bool, BigUint) = (Self::SGN_N[2], Self::COEFF_N[2].into());
+        let (sgn_n22, n22): (bool, BigUint) = (Self::SGN_N[3], Self::COEFF_N[3].into());
+
+        let r: BigUint = Self::ScalarField::MODULUS.into();
+
+        // beta = vector([k,0]) * self.curve.N_inv
+        // The inverse of N is 1/r * Matrix([[n22, -n12], [-n21, n11]]).
+        // so β = (k*n22, -k*n12)/r
+
+        let beta_1 = &scalar * &n22 / &r;
+        let sgn_beta_1 = sgn_n22;
+        let beta_2 = &scalar * &n12 / &r;
+        let sgn_beta_2 = !sgn_n12;
+
+        // b = vector([int(beta[0]), int(beta[1])]) * self.curve.N
+        // b = (β1N11 + β2N21, β1N12 + β2N22) with the signs!
+        //   = (b11   + b12  , b21   + b22)   with the signs!
+
+        // b1
+        let b11 = &beta_1 * &n11;
+        let sgn_b11 = sgn_beta_1 == sgn_n11;
+        let b12 = &beta_2 * &n21;
+        let sgn_b12 = sgn_beta_2 == sgn_n21;
+        let (b1, sgn_b1) = if sgn_b11 == sgn_b12 {
+            ((b11 + b12) % &r, sgn_b11)
+        } else {
+            if b11 > b12 {
+                (b11 - b12, sgn_b11)
+            } else {
+                (b12 - b11, sgn_b12)
+            }
+        };
+
+        // b2
+        let b21 = &beta_1 * &n12;
+        let sgn_b21 = sgn_beta_1 == sgn_n12;
+        let b22 = &beta_2 * &n22;
+        let sgn_b22 = sgn_beta_2 == sgn_n22;
+
+        let (b2, sgn_b2) = if sgn_b21 == sgn_b22 {
+            ((b21 + b22) % &r, sgn_b21)
+        } else {
+            if b21 > b22 {
+                (b21 - b22, sgn_b21)
+            } else {
+                (b22 - b21, sgn_b22)
+            }
+        };
+
+        // k1
+        let (k1, sgn_k1) = if !sgn_b1 {
+            (scalar + b1, true)
+        } else {
+            if scalar > b1 {
+                (scalar - b1, true)
+            } else {
+                (b1 - scalar, false)
+            }
+        };
+
+        // k2
+        let (k2, sgn_k2) = (b2, !sgn_b2);
+
+        (
+            Self::ScalarField::from(k1),
+            sgn_k1,
+            Self::ScalarField::from(k2),
+            sgn_k2,
+        )
+    }
 }
+
+//
+// This is not working
+//
+
+pub trait GLV<T: ScalarMul> {
+    fn endomorphism(p: T) -> T {
+        p
+    }
+
+    fn glv_mul<C: GLVConfig>(p: T, k: C::ScalarField) -> T {
+        let (k1, sgn_k1, k2, sgn_k2) = C::scalar_decomposition(k);
+        let p1 = if sgn_k1 { p } else { -p };
+        let p2 = if sgn_k2 {
+            Self::endomorphism(p)
+        } else {
+            -Self::endomorphism(p)
+        };
+        p1.mul_bigint(k1.into_bigint()) + p2.mul_bigint(k2.into_bigint())
+    }
+}
+
+//     fn glv_mul<V: VariableBaseMSM, C:GLVConfig> (
+//         base: AffineRepr<C>,
+//         scalar: V::ScalarField,
+//     ) {
+//     // ) -> V {
+//         let (k1, sgn_k1, k2, sgn_k2) = C::scalar_decomposition(scalar);
+//         let b1 = if sgn_k1 { base } else {-base};
+//         let mut b2: V::MulBase = C::endomorphism::<V>(base);
+//         b2 = if sgn_k2 { b2 } else {-b2};
+//         let x = b2.mul(k2);
+//         // VariableBaseMSM::msm_bigint(
+//         //     &[b1, b2],
+//         //     &[k1.into_bigint(), k2.into_bigint()],
+//         // )
+//     }
+// }
+
+// /// Performs NAF GLV multiplication.
+// fn naf_glv_mul(
+//     base: &<Self::Curve as CurveGroup>::Affine,
+//     scalar: Self::ScalarField,
+// ) -> Self::Curve {
+//     let (k1, sgn_k1, k2, sgn_k2) = Self::scalar_decomposition(scalar);
+//     variable_base::naf_two_scalar_mul::<Self>(
+//         *base,
+//         k1.into_bigint(),
+//         sgn_k1,
+//         Self::endomorphism(base),
+//         k2.into_bigint(),
+//         sgn_k2,
+//     )
+// }
--- test-curves/src/bls12_381/g1.rs
+++ test-curves/src/bls12_381/g1.rs
@@ -1,7 +1,9 @@
 use crate::bls12_381::*;
+use crate::CurveGroup;
 use ark_ec::{
     hashing::curve_maps::wb::{IsogenyMap, WBConfig},
     models::CurveConfig,
+    scalar_mul::glv::GLVConfig,
     short_weierstrass::{self, *},
 };
 use ark_ff::{MontFp, Zero};
@@ -71,8 +73,27 @@ pub const G1_GENERATOR_X: Fq = MontFp!("3685416753713387016781088315183077757961
 #[rustfmt::skip]
 pub const G1_GENERATOR_Y: Fq = MontFp!("1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569");
 
+impl GLVConfig for Config {
+    const COEFFS_ENDOMORPHISM: &'static[Self::BaseField] = &[
+        MontFp!("793479390729215512621379701633421447060886740281060493010456487427281649075476305620758731620350")
+    ];
+
+    const LAMBDA: Self::ScalarField =
+        MontFp!("52435875175126190479447740508185965837461563690374988244538805122978187051009");
+
+    const COEFF_N: [<Self as CurveConfig>::ScalarField; 4] = [
+        MontFp!("228988810152649578064853576960394133504"),
+        MontFp!("1"),
+        MontFp!("1"),
+        MontFp!("228988810152649578064853576960394133503"),
+    ];
+    const SGN_N: [bool; 4] = [true, true, false, true];
+}
+
 #[cfg(test)]
 mod test {
+    use crate::bls12_381;
+
     use super::*;
     use ark_ec::CurveGroup;
     use ark_std::UniformRand;
@@ -93,5 +114,20 @@ mod test {
 
         let g_s_affine_fast = G1Projective::normalize_batch(&g_s);
         assert_eq!(g_s_affine_naive.as_ref(), g_s_affine_fast.as_slice());
+    }
+
+    #[test]
+    fn test_glv_scalar() {
+        use crate::bls12_381::{g1::Config, Fr};
+        use ark_ec::scalar_mul::glv::GLV;
+        use ark_ec::AffineRepr;
+        let mut rng = ark_std::test_rng();
+        let s = Fr::rand(&mut rng);
+        let (k1, book1, k2, book2) = Config::scalar_decomposition(s);
+        assert_eq!(k1 - Config::LAMBDA * k2, s);
+        let g = Affine::<Config>::generator();
+        let s_g = g * s;
+        // this is not working
+        // assert_eq!(s_g, GLV::<G1Projective>::glv_mul(g,s));
     }
 }
