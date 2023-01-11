3a8acbada7e10c75e03b7d0c8250998b1bfa020c
Simon Masson <simon.masson@yahoo.fr>
2023-01-11 17:36:23 +0000

    glv bench added

--- bench-templates/src/macros/mod.rs
+++ bench-templates/src/macros/mod.rs
@@ -43,6 +43,37 @@ macro_rules! bench {
     };
     (
         Name = $name:expr,
+        Pairing = $Pairing:ident,
+        G1 = $G1:ident,
+        G2 = $G2:ident,
+        ScalarField = $Fr:ident,
+        G1BaseField = $Fq:ident,
+        G2BaseField = $FqExt:ident,
+        TargetField = $FqTarget:ident,
+    ) => {
+        $crate::ec_bench!($name, $G1);
+        $crate::ec_bench!($name, $G2);
+        $crate::f_bench!(prime, $name, $Fr);
+        $crate::f_bench!(prime, $name, $Fq);
+        $crate::f_bench!(extension, $name, $FqExt);
+        $crate::f_bench!(target, $name, $FqTarget);
+        $crate::pairing_bench!($Pairing);
+        $crate::glv_bench!($name, $G1);
+        $crate::glv_bench!($name, $G2);
+
+        paste! {
+            criterion_main!(
+                [<$G1:lower>]::benches,
+                [<$G2:lower>]::benches,
+                [<$Fr:lower>]::benches,
+                [<$Fq:lower>]::benches,
+                [<$FqExt:lower>]::benches,
+                [<$FqTarget:lower>]::benches
+            );
+        }
+    };
+    (
+        Name = $name:expr,
         Group = $G:ident,
         ScalarField = $Fr:ident,
         BaseField = $Fq:ident,
