c7ffca17c4d5c1ddd18e5ce00ac9331d4964bfd1
¨Jeff <burdges@gnunet.org>
2023-03-21 20:35:57 +0100

    Again rustfmt

--- ec/src/hashing/curve_maps/swu.rs
+++ ec/src/hashing/curve_maps/swu.rs
@@ -261,7 +261,7 @@ mod test {
         let mut map_range: Vec<Affine<TestSWUMapToCurveConfig>> = vec![];
         for current_field_element in 0..127 {
             let point = F127::from(current_field_element as u64);
-            map_range.push( SWUMap::<TestSWUMapToCurveConfig>::map_to_curve(point).unwrap() );
+            map_range.push(SWUMap::<TestSWUMapToCurveConfig>::map_to_curve(point).unwrap());
         }
 
         let mut counts = HashMap::new();
