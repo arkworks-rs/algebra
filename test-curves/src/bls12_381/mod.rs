c4e67cdcbc962f0f784d43e36944a6f93a55b460
weikengchen <w.k@berkeley.edu>
2023-08-03 20:17:47 -0700

    avoid importing config twice

--- test-curves/src/bls12_381/mod.rs
+++ test-curves/src/bls12_381/mod.rs
@@ -18,7 +18,16 @@ pub mod g2;
 #[cfg(feature = "bls12_381_curve")]
 pub mod g2_swu_iso;
 #[cfg(feature = "bls12_381_curve")]
-pub use {fq::*, fq12::*, fq2::*, fq6::*, g1::*, g1_swu_iso::*, g2::*, g2_swu_iso::*};
+pub use {
+    fq::*,
+    fq12::*,
+    fq2::*,
+    fq6::*,
+    g1::{G1Affine, G1Projective},
+    g1_swu_iso::*,
+    g2::{G2Affine, G2Projective},
+    g2_swu_iso::*,
+};
 
 #[cfg(test)]
 mod tests;
