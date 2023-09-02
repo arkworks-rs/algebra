cbcdf644a6d1c0062815a7bf1245f6e142954af6
mmagician <marcin.gorny.94@protonmail.com>
2023-09-02 23:12:15 +0200

    Replace pairing with multi-pairing bench, up to 10 pairs

--- bench-templates/src/macros/pairing.rs
+++ bench-templates/src/macros/pairing.rs
@@ -73,54 +73,8 @@ macro_rules! pairing_bench {
                         })
                     },
                 );
-                pairing.bench_function(&format!("Full Pairing for {}", stringify!($curve)), |b| {
-                    b.iter(|| {
-                        i = (i + 1) % SAMPLES;
-                        <$curve as Pairing>::multi_pairing([g1s[i]], [g2s[i]])
-                    })
-                });
-            }
 
-            $crate::criterion_group!(benches, pairing);
-        }
-    };
-}
-
-#[macro_export]
-macro_rules! multi_pairing_bench {
-    ($curve:ident) => {
-        mod multi_pairing {
-            use super::*;
-            use ark_std::UniformRand;
-            fn pairing(c: &mut $crate::criterion::Criterion) {
-                use ark_ec::{pairing::Pairing, CurveGroup};
-
-                type G1 = <$curve as Pairing>::G1;
-                type G2 = <$curve as Pairing>::G2;
-                type G1Prepared = <$curve as Pairing>::G1Prepared;
-                type G2Prepared = <$curve as Pairing>::G2Prepared;
-
-                const SAMPLES: usize = 1000;
-
-                let mut rng = ark_std::test_rng();
-
-                let g1s = (0..SAMPLES).map(|_| G1::rand(&mut rng)).collect::<Vec<_>>();
-                let g2s = (0..SAMPLES).map(|_| G2::rand(&mut rng)).collect::<Vec<_>>();
-                let g1s = G1::normalize_batch(&g1s);
-                let g2s = G2::normalize_batch(&g2s);
-                let (prepared_1, prepared_2): (Vec<G1Prepared>, Vec<G2Prepared>) = g1s
-                    .iter()
-                    .zip(&g2s)
-                    .map(|(g1, g2)| {
-                        let g1: G1Prepared = g1.into();
-                        let g2: G2Prepared = g2.into();
-                        (g1, g2)
-                    })
-                    .unzip();
-                let mut i = 0;
-                let mut pairing = c.benchmark_group(format!("Pairing for {}", stringify!($curve)));
-                
-                const NUM_PAIRS: usize = 5;
+                const NUM_PAIRS: usize = 10;
                 
                 for pairs in 1..=NUM_PAIRS {
                     pairing.bench_function(&format!("Multi Pairing for {} with {} pairs", stringify!($curve), pairs), |b| {
@@ -139,4 +93,3 @@ macro_rules! multi_pairing_bench {
         }
     };
 }
-
