fc4921cd4def2bd83ff981811a2902394ad0f3d8
mmagician <marcin.gorny.94@protonmail.com>
2023-11-20 10:20:28 +0100

    enable parallel execution for `make_digits` (#703)

--- ec/src/scalar_mul/variable_base/mod.rs
+++ ec/src/scalar_mul/variable_base/mod.rs
@@ -102,6 +102,12 @@ fn msm_bigint_wnaf<V: VariableBaseMSM>(
 
     let num_bits = V::ScalarField::MODULUS_BIT_SIZE as usize;
     let digits_count = (num_bits + c - 1) / c;
+    #[cfg(feature = "parallel")]
+    let scalar_digits = scalars
+        .into_par_iter()
+        .flat_map_iter(|s| make_digits(s, c, num_bits))
+        .collect::<Vec<_>>();
+    #[cfg(not(feature = "parallel"))]
     let scalar_digits = scalars
         .iter()
         .flat_map(|s| make_digits(s, c, num_bits))
