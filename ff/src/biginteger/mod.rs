320ddb96834a3f1d22902d708e976fe31091138c
hdvanegasm <hdvanegasm@gmail.com>
2023-12-20 10:42:32 -0500

    Added documentation and corrected CHANGELOG.md

--- ff/src/biginteger/mod.rs
+++ ff/src/biginteger/mod.rs
@@ -732,6 +732,11 @@ impl<B: Borrow<Self>, const N: usize> BitOr<B> for BigInt<N> {
 impl<const N: usize> Shr<u32> for BigInt<N> {
     type Output = Self;
 
+    /// Computes bitwise shift right operation.
+    /// 
+    /// Differently from the built-in numeric types (u8, u32, u64, etc.) this
+    /// operation does *not* return an underflow error if the number of bits
+    /// shifted is larger than N * 64.
     fn shr(self, mut rhs: u32) -> Self::Output {
         let mut data = self.0.clone();
 
