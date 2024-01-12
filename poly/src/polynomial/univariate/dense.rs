7ad0e73e7068502390dc1ed718ccf566ad9e8106
Pratyush Mishra <pratyushmishra@berkeley.edu>
2024-01-11 22:57:08 -0500

    Better tests

--- poly/src/polynomial/univariate/dense.rs
+++ poly/src/polynomial/univariate/dense.rs
@@ -938,13 +938,20 @@ mod tests {
     fn evaluate_over_domain_test() {
         let rng = &mut ark_std::test_rng();
         let domain = crate::domain::Radix2EvaluationDomain::<Fr>::new(1 << 10).unwrap();
+        let offset = Fr::GENERATOR;
+        let coset = domain.get_coset(offset).unwrap();
         for _ in 0..100 {
             let poly = DensePolynomial::<Fr>::rand(1 << 11, rng);
             let evaluations = domain
                 .elements()
                 .map(|e| poly.evaluate(&e))
                 .collect::<Vec<_>>();
-            assert_eq!(evaluations, poly.evaluate_over_domain(domain).evals);
+            assert_eq!(evaluations, poly.evaluate_over_domain_by_ref(domain).evals);
+            let evaluations = coset
+                .elements()
+                .map(|e| poly.evaluate(&e))
+                .collect::<Vec<_>>();
+            assert_eq!(evaluations, poly.evaluate_over_domain(coset).evals);
         }
         let zero = DensePolynomial::zero();
         let evaluations = domain
@@ -956,20 +963,19 @@ mod tests {
 
     use crate::{univariate::DensePolynomial, DenseUVPolynomial, Radix2EvaluationDomain};
 
-    #[derive(MontConfig)]
-    #[modulus = "18446744069414584321"]
-    #[generator = "7"]
-    pub struct FrConfig64;
-    pub type Field64 = Fp64<MontBackend<FrConfig64, 1>>;
-
     #[test]
-    fn test_eval_over_domain() {
-        type F = Field64;
+    fn evaluate_over_domain_regression_test() {
+        // See https://github.com/arkworks-rs/algebra/issues/745
+        #[derive(MontConfig)]
+        #[modulus = "18446744069414584321"]
+        #[generator = "7"]
+        struct FrConfig64;
+        type F = Fp64<MontBackend<FrConfig64, 1>>;
+
         let degree = 17;
         let eval_domain_size = 16;
 
         let poly = DensePolynomial::from_coefficients_vec(vec![F::ONE; degree]);
-        dbg!(poly.degree());
         let domain = Radix2EvaluationDomain::new(eval_domain_size).unwrap();
 
         // Now we get a coset
