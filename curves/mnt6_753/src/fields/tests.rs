2340b82d39ca90dfec405a524df84632e8e6412a
mmagician <marcin.gorny.94@protonmail.com>
2024-01-27 16:06:55 +0100

    decrease iterations for mnt6_753

--- curves/mnt6_753/src/fields/tests.rs
+++ curves/mnt6_753/src/fields/tests.rs
@@ -3,10 +3,10 @@ use ark_algebra_test_templates::*;
 use ark_ff::fields::{models::fp6_2over3::*, quadratic_extension::QuadExtConfig};
 use ark_std::{rand::Rng, test_rng};
 
-test_field!(fr; Fr; mont_prime_field);
-test_field!(fq; Fq; mont_prime_field);
-test_field!(fq3; Fq3);
-test_field!(fq6; Fq6);
+test_field!(fr; Fr; 200; mont_prime_field);
+test_field!(fq; Fq; 200; mont_prime_field);
+test_field!(fq3; 200; Fq3);
+test_field!(fq6; 200; Fq6);
 
 #[test]
 fn test_fq3_more() {
