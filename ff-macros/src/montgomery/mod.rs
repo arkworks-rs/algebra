8977a7e3e0229ad2d3727f2fa668eb634554129c
Pratyush Mishra <pratyushmishra@berkeley.edu>
2024-02-27 11:37:44 -0500

    tweak

--- ff-macros/src/montgomery/mod.rs
+++ ff-macros/src/montgomery/mod.rs
@@ -100,9 +100,8 @@ pub fn mont_config_helper(
         quote::quote! {}
     };
 
-    let scope_name = format_ident!("{}___", config_name.to_string().to_lowercase());
     quote::quote! {
-        const #scope_name: () = {
+        const _: () = {
             use ark_ff::{fields::Fp, BigInt, BigInteger, biginteger::arithmetic as fa, fields::*};
             type B = BigInt<#limbs>;
             type F = Fp<MontBackend<#config_name, #limbs>, #limbs>;
