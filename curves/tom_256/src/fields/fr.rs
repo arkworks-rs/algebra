f90020f8e76f37f2107ebe683ab9ced58a64d7e6
lovesh <lovesh.bond@gmail.com>
2025-01-29 17:46:36 +0530

    use correct Fq and Fr for tom-256 and remove overridden method
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- curves/tom_256/src/fields/fr.rs
+++ curves/tom_256/src/fields/fr.rs
@@ -1,9 +1,9 @@
 use ark_ff::fields::{Fp256, MontBackend, MontConfig};
 
 #[derive(MontConfig)]
-#[modulus = "115792089210356248762697446949407573530594504085698471288169790229257723883799"]
-#[generator = "7"]
-#[small_subgroup_base = "3"]
-#[small_subgroup_power = "1"]
+#[modulus = "115792089210356248762697446949407573530086143415290314195533631308867097853951"]
+#[generator = "6"]
+// #[small_subgroup_base = "3"]
+// #[small_subgroup_power = "1"]
 pub struct FrConfig;
 pub type Fr = Fp256<MontBackend<FrConfig, 4>>;
