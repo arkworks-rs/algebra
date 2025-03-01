f8781e771bf20197d2c86a2b45c7f75c01496c34
Chanyang <124057018+woojucy@users.noreply.github.com>
2025-03-02 03:02:39 +0900

    Update poly/src/polynomial/univariate/sparse.rs
    Co-authored-by: Andrew Zitek-Estrada <1497456+z-tech@users.noreply.github.com>

--- poly/src/polynomial/univariate/sparse.rs
+++ poly/src/polynomial/univariate/sparse.rs
@@ -247,6 +247,12 @@ pub enum CoeffVecError {
     DuplicateDegree,
 }
 
+#[derive(Debug)]
+pub enum CoeffVecError {
+    ZeroCoefficient,
+    DuplicateDegree,
+}
+
 impl<F: Field> SparsePolynomial<F> {
 
     // Function to validate that the input coefficient vector is simplified
