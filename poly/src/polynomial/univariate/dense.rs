df47f740715135b64070ca637c2cc5dc367a3405
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2025-06-20 08:17:58 +0200

    refactor `DensePolynomial` add (#905)
    * refactor DensePolynomial add
    
    * Use parallel iteration
    
    ---------
    
    Co-authored-by: Pratyush Mishra <pratyushmishra@berkeley.edu>
    Co-authored-by: Pratyush Mishra <pratyush795@gmail.com>

--- poly/src/polynomial/univariate/dense.rs
+++ poly/src/polynomial/univariate/dense.rs
@@ -284,32 +284,35 @@ impl<'a, F: Field> Add<&'a DensePolynomial<F>> for &DensePolynomial<F> {
     type Output = DensePolynomial<F>;
 
     fn add(self, other: &'a DensePolynomial<F>) -> DensePolynomial<F> {
-        let mut result = if self.is_zero() {
-            other.clone()
-        } else if other.is_zero() {
-            self.clone()
-        } else if self.degree() >= other.degree() {
-            let mut result = self.clone();
-            result
-                .coeffs
-                .iter_mut()
-                .zip(&other.coeffs)
-                .for_each(|(a, b)| {
-                    *a += b;
-                });
-            result
+        // If the first polynomial is zero, the result is simply the second polynomial.
+        if self.is_zero() {
+            return other.clone();
+        }
+
+        // If the second polynomial is zero, the result is simply the first polynomial.
+        if other.is_zero() {
+            return self.clone();
+        }
+
+        // Determine which polynomial has the higher degree.
+        let (longer, shorter) = if self.degree() >= other.degree() {
+            (self, other)
         } else {
-            let mut result = other.clone();
-            result
-                .coeffs
-                .iter_mut()
-                .zip(&self.coeffs)
-                .for_each(|(a, b)| {
-                    *a += b;
-                });
-            result
+            (other, self)
         };
+
+        // Start with a copy of the longer polynomial as the base for the result.
+        let mut result = longer.clone();
+
+        // Iterate through the coefficients of the `shorter` polynomial.
+        // Add them to the corresponding coefficients in the `longer` polynomial.
+        cfg_iter_mut!(result)
+            .zip(&shorter.coeffs)
+            .for_each(|(a, b)| *a += b);
+
+        // Remove any trailing zeros from the resulting polynomial.
         result.truncate_leading_zeros();
+
         result
     }
 }
