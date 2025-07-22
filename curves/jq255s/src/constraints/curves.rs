229a14a9cf986209263ba8594869b212fcdf3b99
prismaman <svdv2000@gmail.com>
2025-07-22 12:33:06 +0200

    fix: Add clarifying documentation and missing refs

--- curves/jq255s/src/constraints/curves.rs
+++ curves/jq255s/src/constraints/curves.rs
@@ -1,5 +1,5 @@
 use crate::{constraints::FqVar, *};
 use ark_r1cs_std::groups::curves::short_weierstrass::ProjectiveVar;
 
-/// A group element in the Curve25519 curve.
+/// A group element in the Jq255s curve.
 pub type GVar = ProjectiveVar<Config, FqVar>;
