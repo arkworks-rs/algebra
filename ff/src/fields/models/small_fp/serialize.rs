e39da817f681804623695d0ec915efc950948cf6
benbencik <bencik42@gmail.com>
2025-10-04 11:29:54 +0200

    consider (de)serialization of small elements

--- ff/src/fields/models/small_fp/serialize.rs
+++ ff/src/fields/models/small_fp/serialize.rs
@@ -1,9 +1,10 @@
 use crate::fields::models::small_fp::small_fp_backend::{SmallFp, SmallFpConfig};
-use crate::{PrimeField, Zero};
+use crate::{BigInt, PrimeField, Zero};
 use ark_serialize::{
     buffer_byte_size, CanonicalDeserialize, CanonicalDeserializeWithFlags, CanonicalSerialize,
     CanonicalSerializeWithFlags, Compress, EmptyFlags, Flags, SerializationError, Valid, Validate,
 };
+use ark_std::vec;
 
 impl<P: SmallFpConfig> CanonicalSerializeWithFlags for SmallFp<P> {
     fn serialize_with_flags<W: ark_std::io::Write, F: Flags>(
@@ -21,16 +22,29 @@ impl<P: SmallFpConfig> CanonicalSerializeWithFlags for SmallFp<P> {
         // serialized with `flags`. If `F::BIT_SIZE < 8`,
         // this is at most `N * 8 + 1`
         let output_byte_size = buffer_byte_size(Self::MODULUS_BIT_SIZE as usize + F::BIT_SIZE);
+        let mut w = writer;
 
         // Write out `self` to a temporary buffer.
         // The size of the buffer is $byte_size + 1 because `F::BIT_SIZE`
         // is at most 8 bits.
-        let mut bytes = crate::const_helpers::SerBuffer::zeroed();
-        bytes.copy_from_u64_slice(&self.into_bigint().0);
-        // Mask out the bits of the last byte that correspond to the flag.
-        bytes[output_byte_size - 1] |= flags.u8_bitmask();
 
-        bytes.write_up_to(writer, output_byte_size)?;
+        if output_byte_size <= 8 {
+            // Fields with type smaller than u64
+            // Writes exactly the minimal required number of bytes
+            let bigint = self.into_bigint();
+            let value = bigint.0[0];
+            let mut bytes = [0u8; 8];
+            bytes.copy_from_slice(&value.to_le_bytes());
+            bytes[output_byte_size - 1] |= flags.u8_bitmask();
+            w.write_all(&bytes[..output_byte_size])?;
+        } else {
+            // For larger fields, use the approach from `FpConfig`
+            let mut bytes = crate::const_helpers::SerBuffer::zeroed();
+            bytes.copy_from_u64_slice(&self.into_bigint().0);
+            // Mask out the bits of the last byte that correspond to the flag.
+            bytes[output_byte_size - 1] |= flags.u8_bitmask();
+            bytes.write_up_to(&mut w, output_byte_size)?;
+        }
         Ok(())
     }
 
@@ -71,16 +85,36 @@ impl<P: SmallFpConfig> CanonicalDeserializeWithFlags for SmallFp<P> {
         // Calculate the number of bytes required to represent a field element
         // serialized with `flags`.
         let output_byte_size = Self::zero().serialized_size_with_flags::<F>();
+        let mut r = reader;
 
-        let mut masked_bytes = crate::const_helpers::SerBuffer::zeroed();
-        masked_bytes.read_exact_up_to(reader, output_byte_size)?;
-        let flags = F::from_u8_remove_flags(&mut masked_bytes[output_byte_size - 1])
-            .ok_or(SerializationError::UnexpectedFlags)?;
+        if output_byte_size <= 8 {
+            // Fields with type smaller than u64
+            let mut bytes = vec![0u8; output_byte_size];
+            r.read_exact(&mut bytes)?;
+            let flags = F::from_u8_remove_flags(&mut bytes[output_byte_size - 1])
+                .ok_or(SerializationError::UnexpectedFlags)?;
 
-        let self_integer = masked_bytes.to_bigint();
-        Self::from_bigint(self_integer)
-            .map(|v| (v, flags))
-            .ok_or(SerializationError::InvalidData)
+            let mut limb_bytes = [0u8; 8];
+            limb_bytes[..output_byte_size.min(8)]
+                .copy_from_slice(&bytes[..output_byte_size.min(8)]);
+            let limb = u64::from_le_bytes(limb_bytes);
+            let bigint = BigInt::<2>::new([limb, 0]);
+
+            Self::from_bigint(bigint)
+                .map(|v| (v, flags))
+                .ok_or(SerializationError::InvalidData)
+        } else {
+            // For larger fields, use the approach from `FpConfig`
+            let mut masked_bytes = crate::const_helpers::SerBuffer::zeroed();
+            masked_bytes.read_exact_up_to(&mut r, output_byte_size)?;
+            let flags = F::from_u8_remove_flags(&mut masked_bytes[output_byte_size - 1])
+                .ok_or(SerializationError::UnexpectedFlags)?;
+
+            let self_integer = masked_bytes.to_bigint();
+            Self::from_bigint(self_integer)
+                .map(|v| (v, flags))
+                .ok_or(SerializationError::InvalidData)
+        }
     }
 }
 
