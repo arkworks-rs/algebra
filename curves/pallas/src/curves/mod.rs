97276601034acde91937f8c3a31d8af61e8f3050
lovesh <lovesh.bond@gmail.com>
2026-07-08 18:46:11 +0530

    Use GLV for Pasta and bugfix in Bucket to Affine conversion

--- curves/pallas/src/curves/mod.rs
+++ curves/pallas/src/curves/mod.rs
@@ -2,6 +2,7 @@ use ark_ec::{
     models::CurveConfig,
     scalar_mul::glv::GLVConfig,
     short_weierstrass::{self as sw, SWCurveConfig},
+    AffineRepr,
 };
 use ark_ff::{AdditiveGroup, BigInt, Field, MontFp, PrimeField, Zero};
 
@@ -47,6 +48,18 @@ impl SWCurveConfig for PallasConfig {
     #[inline(always)]
     fn mul_by_a(_: Self::BaseField) -> Self::BaseField {
         Self::BaseField::zero()
+    }
+
+    #[inline]
+    fn mul_projective(base: &Projective, scalar: &[u64]) -> Projective {
+        let s = Self::ScalarField::from_sign_and_limbs(true, scalar);
+        GLVConfig::glv_mul_projective(*base, s)
+    }
+
+    #[inline]
+    fn mul_affine(base: &Affine, scalar: &[u64]) -> Projective {
+        let s = Self::ScalarField::from_sign_and_limbs(true, scalar);
+        <Self as GLVConfig>::glv_mul_projective(base.into_group(), s)
     }
 }
 
